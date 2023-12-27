27524810105676cc1ad086c818b9ec84793726cd
emy <emy@ubuntu20045.myguest.virtualbox.org>
2023-12-27 16:28:49 +0100

    internet gw

---
+++ PBL-project-17/terraform.tfvars
@@ -0,0 +1,24 @@
+name = "emydevops"
+
+region = "us-east-1"
+
+vpc_cidr = "172.16.0.0/16" 
+
+enable_dns_support = "true" 
+
+enable_dns_hostnames = "true"  
+
+enable_classiclink = "false" 
+
+enable_classiclink_dns_support = "false" 
+
+preferred_number_of_public_subnets = 2
+
+preferred_number_of_private_subnets = 4
+
+tags = {
+  Environment     = "emy-devops" 
+  Owner-Email     = "ojoiimmanuel@gmail.com"
+  Managed-By      = "Terraform"
+  Billing-Account = "1234567890"
+}
---
+++ PBL-project-17/variables.tf
@@ -0,0 +1,44 @@
+variable "name"{
+    type = string
+    default = "emydevops"
+}
+
+variable "region"{
+  default = "us-east-1"
+}
+
+variable "vpc_cidr" {
+  default = "172.16.0.0/16"  
+}
+
+
+variable "enable_dns_support" {
+    default = "true"
+}
+
+variable "enable_dns_hostnames" {
+    default ="true" 
+}
+
+variable "enable_classiclink" {
+    default = "false"
+}
+
+variable "enable_classiclink_dns_support" {
+    default = "false"
+}
+
+variable "preferred_number_of_public_subnets" {
+  default = 2
+}
+
+variable "preferred_number_of_private_subnets" {
+  default = 4
+}
+
+variable "tags" {
+    description = "A mapping of tags to assign to all resources."
+    type = map(string)
+    default = {}
+}
+  
